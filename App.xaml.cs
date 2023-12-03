9f54229efa0b95458d58e6d7baaa754826f80497
Raluca Moldovan <ralu.adelina2@gmail.com>
2023-12-03 20:07:30 +0200

    lab9

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,24 @@
 ﻿namespace Moldovan__Raluca_lab7;
-
+using System;
+using System.IO;
+using Moldovan__Raluca_lab7.Data;
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+    public App()
 	{
 		InitializeComponent();
 
